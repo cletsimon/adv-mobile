74b3f71f1d2fae92535f13d14d4ae2e7e17f4cb3
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-01 05:47:18 +0800

    wiring up signup, signin, and signout

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,31 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = dispatch => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+    // if we signup, modify our state, and say that we are authenticated
+    // if signing up fails, we probably need to reflect an error message
+    //somewhere
+  };
+};
+
+const signin = dispatch => {
+  return ({ email, password }) => {
+    // try to signin
+    // handle success by updating state
+    // handle failure by showing error message somehow
+  };
+};
+
+const signout = dispatch => {
+  return () => {
+    // somehow sign out!!
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signup, signin, signout },
   { isSignedIn: false }
 );

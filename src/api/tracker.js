742b8978cf2294886ca68db5c0761f1fca12a126
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-01 06:13:01 +0800

    [signup] send date to api

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://7df3b75c.ngrok.io"
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Spacer>
@@ -27,8 +30,11 @@ const SignupScreen = ({ navigation }) => {
         autoCapitlize="none"
         autoCorrect={false}
       />
+      {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
     </View>
   );
@@ -45,6 +51,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     marginBottom: 250
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: "red",
+    marginLeft: 15,
+    marginTop: 15
   }
 });
 

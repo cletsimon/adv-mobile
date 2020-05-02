40c62338e0766e1e13dee46f7a98da6657460155
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-02 14:24:08 +0800

    disable tracking

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,19 +2,21 @@ import "../_mockLocation";
 import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
-import { SafeAreaView, NavigationEvents } from "react-navigation";
+import { SafeAreaView, withNavigationFocus } from "react-navigation";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(addLocation);
+  const [err] = useLocation(isFocused, addLocation);
+
+  console.log(isFocused);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a track</Text>
       <Map />
-      <NavigationEvents onWillBlur={() => console.log("leaving")} />
       {err ? <Text> Please enable location services</Text> : null}
     </SafeAreaView>
   );
@@ -22,4 +24,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);

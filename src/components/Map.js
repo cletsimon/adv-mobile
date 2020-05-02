2bcbcd51348315e95ca9169c1833049d87cd4f21
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-02 16:18:48 +0800

    bring back the polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,14 +5,13 @@ import { Context as LocationContext } from "../context/LocationContext";
 
 const Map = () => {
   const {
-    state: { currentLocation }
+    state: { currentLocation, locations }
   } = useContext(LocationContext);
 
   if (!currentLocation) {
     return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
 
-  console.log(currentLocation);
   return (
     <MapView
       style={styles.map}
@@ -33,6 +32,7 @@ const Map = () => {
         strokeColor="rgba(158,158,255,1.0)"
         fillColor="rgba(158,158,255,0.3)"
       />
+      <Polyline coordinates={locations.map(loc => loc.coords)} />
     </MapView>
   );
 };

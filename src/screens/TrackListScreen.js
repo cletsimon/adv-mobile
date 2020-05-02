c340bad0ccc45cd889eb873bf33de39bb6f54628
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-02 17:48:59 +0800

    add titles to screens

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -9,7 +9,6 @@ const TrackListScreen = ({ navigation }) => {
   return (
     <>
       <NavigationEvents onWillFocus={fetchTracks} />
-      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
       <FlatList
         data={state}
         keyExtractor={item => item._id}
@@ -27,6 +26,10 @@ const TrackListScreen = ({ navigation }) => {
       />
     </>
   );
+};
+
+TrackListScreen.navigationOptions = {
+  title: "Tracks"
 };
 
 const styles = StyleSheet.create({});

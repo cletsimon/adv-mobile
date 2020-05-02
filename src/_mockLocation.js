9de6973ec66a4effc593991fdecf18e097277826
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-02 13:25:06 +0800

    ocoation context

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,10 @@ const getLocation = increment => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees
+      //longitude: -122.0312186 + increment * tenMetersWithDegrees,
+      //latitude: 37.33233141 + increment * tenMetersWithDegrees
+      longitude: 10.30093355 + increment * tenMetersWithDegrees,
+      latitude: 123.87800356 + increment * tenMetersWithDegrees
     }
   };
 };
